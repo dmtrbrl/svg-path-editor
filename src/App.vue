30609f3dabefc99e34b6fc694903ce780667b94c
Dmytro <bdpochta@gmail.com>
2018-03-30 10:46:49 +0300

    Init vue app

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+    <h1 class="heading">SVG Path Editor</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+@import url('https://fonts.googleapis.com/css?family=Roboto:400,500');
+body{
+  padding: 0;
+  margin: 0;
+  background: #393939;
+  font-family: 'Roboto', sans-serif;
+}
+h1, h2, h3, h4, h5, h6, strong, b{
+  font-weight: 500;
+}
+.heading{
+  color: #FFFCEF;
+  padding: 20px;
+  margin: 0;
+  font-size: 24px;
+  line-height: 1;
+  
+}
+</style>
